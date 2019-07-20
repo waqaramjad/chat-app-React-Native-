aba5ba32a30fe3e251068a3c9c5816e5656312e6
waqaramjad <waqaramjad345@gmail.com>
2019-07-20 20:48:32 +0500

    add chat module

--- components/resource/FirebaseSvc.js
+++ components/resource/FirebaseSvc.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 import uuid from 'uuid';
 
 const config = {
-  apiKey: "AIzaSyD6WKr4nBqVaGpVhjBEQ2v1FEDqWuLheko",
-  authDomain: "letscodescv-5dbc0.firebaseapp.com",
-  databaseURL: "https://letscodescv-5dbc0.firebaseio.com",
-  projectId: "letscodescv-5dbc0",
-  storageBucket: "letscodescv-5dbc0.appspot.com",
-  messagingSenderId: "313319506594"
+  apiKey: "AIzaSyDcyZcVQP8nuHcMJsKd5wHxoaerUW6apZQ",
+  authDomain: "waqarchatapp.firebaseapp.com",
+  databaseURL: "https://waqarchatapp.firebaseio.com",
+  projectId: "waqarchatapp",
+  storageBucket: "waqarchatapp.appspot.com",
+  messagingSenderId: "676235345078",
 }
 
 class FirebaseSvc {
